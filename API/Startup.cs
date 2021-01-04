8ffcfa686d267e592dc72930c95dccc73cbc47e6
Jonathan Corea <jacorea410@gmail.com>
2021-01-04 14:01:44 -0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,11 @@ namespace API
                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -50,6 +55,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
